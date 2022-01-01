a03e91908d144726af81fe86fdb27c41092147dd
David McLeish <dave@mcleish.id.au>
2022-01-01 16:29:27 +1100

    tables

--- mkdoc.py
+++ mkdoc.py
@@ -1,4 +1,5 @@
 WORDFILE = "words.txt"
+SIX = range(6)
 
 def main():
     with open(WORDFILE) as f:
@@ -6,27 +7,28 @@ def main():
         assert(len(words) == 6 ** 4)
     word = iter(words)
     
-    print(r"\documentclass{minimal}")
-    print(r"\usepackage{multicol}")
+    print(r"\documentclass[a4paper,10pt,]{article}")
+    print(r"\usepackage[lmargin=3cm]{geometry}")
+    print(r"\pagestyle{empty}")
     print(r"\begin{document}")
+    print(r"\begin{flushleft}")
 
-    for page in range(6):
-        for tabrow in range(3):
-            print(r"\begin{multicols}{2}")
-            for tabcol in range(2):
-                print(r"\begin{tabular}{ |c|c|c|c|c|c| }")
-                print(r"\hline")
-                for row in range(6):
-                    print(*(next(word) for i in range(6)), sep=" & ", end=" \\\\\n")
-                print(r"\hline")
-                print(r"\end{tabular}")
-                if tabcol == 0:
-                    print(r"\columnbreak")
-
-            print(r"\end{multicols}")
+    for page in SIX:
+        for tabcol in SIX:
+            tabwords = [[next(word) for _ in SIX] for _ in SIX]
+            print(r"\begin{tabular}{ |" + "|".join(["p{6em}"] * 6) + "| }")
+            print(r"\hline")
+            print(*(r"\textbf{%d%d%d}" % (page+1, tabcol+1, i+1) for i in SIX), sep=" & ", end=" \\\\\n")
+            print(r"\hline")
+            for tabrow in SIX:
+                print(*(r"\textbf{%d} %s" % (tabrow+1, tabwords[i][tabrow]) for i in SIX), sep=" & ", end=" \\\\\n")
+            print(r"\hline")
+            print(r"\end{tabular}")
+            print(r"\\")
 
         print(r"\newpage")
 
+    print(r"\end{flushleft}")
     print(r"\end{document}")
 
 
